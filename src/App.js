8d76f526ad82e328a03112ea9950d1914b3c0698
Diego Chapedelaine <25375651+diegochappedelaine@users.noreply.github.com>
2021-11-13 13:51:22 +0100

    add movie page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
-import { Fragment, React } from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Fragment, React, useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useParams,
+} from "react-router-dom";
+import axios from "axios";
 
-import AddMovie from './pages/AddMovie';
-import EditMovie from './pages/EditMovie';
-import MovieDetails from './pages/MovieDetails';
-import MoviesList from './pages/MoviesList';
-import Navbar from './components/Navbar';
-import './style/CSS/style.css'
+import AddMovie from "./pages/AddMovie";
+import EditMovie from "./pages/EditMovie";
+import MovieDetails from "./pages/MovieDetails";
+import MoviesList from "./pages/MoviesList";
+import Navbar from "./components/Navbar";
+import "./style/CSS/style.css";
 
-import Test from './Test';
+import Test from "./Test";
 
+const MoviePage = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const result = await axios(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=6d297bdaca2dc66c4fe66393850480f4&language=fr`
+      );
+      setMovie(result.data);
+    };
+    fetchMovie();
+  }, [id]);
+
+  if (!movie) return <p>No movie</p>;
+
+  return <div>{movie.title}</div>;
+};
 
 function App() {
-
   return (
     <Fragment>
       <Router>
         <Navbar />
-        <main className='ctnr'>
+        <main className="ctnr">
           <Test />
           <Switch>
-            <Route exact path='/AddMovie' component={AddMovie} />
-            <Route path='/MoviesList' component={MoviesList} />
-            <Route path='/MovieDetails/:id' component={MovieDetails} />
-            <Route exact path='/EditMovie' component={EditMovie} />
+            <Route exact path="/movie/:id" component={MoviePage} />
+            <Route exact path="/AddMovie" component={AddMovie} />
+            <Route path="/MoviesList" component={MoviesList} />
+            <Route path="/MovieDetails/:id" component={MovieDetails} />
+            <Route exact path="/EditMovie" component={EditMovie} />
           </Switch>
         </main>
       </Router>
--- src/components/SearchMovie.jsx
+++ src/components/SearchMovie.jsx
@@ -1,90 +1,93 @@
-import { Fragment, React, useEffect, useState } from 'react';
-import axios from 'axios';
-
+import { Fragment, React, useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const SearchMovie = () => {
+  const [searchByTitle, setSearchByTitle] = useState("");
+  const [searchByReleaseYear, setSearchByReleaseYear] = useState("");
+  const [data, setData] = useState("");
+  const [targetId, setTargetId] = useState(null);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
-    const [searchByTitle, setSearchByTitle] = useState('');
-    const [searchByReleaseYear, setSearchByReleaseYear] = useState('');
-    const [data, setData] = useState('');
-    const [targetId, setTargetId] = useState(null);
-    const [selectedMovie, setSelectedMovie] = useState(null);
+  const addData = (e) => {
+    setTargetId(e.target.id);
+    console.log(targetId);
+    // .then(async (targetId) => {
+    //     await axios(
+    //         `https://api.themoviedb.org/3/search/movie?api_key=6d297bdaca2dc66c4fe66393850480f4&language=fr&query=${targetId}&page=1&include_adult=false&primary_release_year=${searchByReleaseYear}`
+    //     );
+    // })
+  };
 
-    const addData = (e) => {
-        setTargetId(e.target.id)
-        console.log(targetId)
-        // .then(async (targetId) => {
-        //     await axios(
-        //         `https://api.themoviedb.org/3/search/movie?api_key=6d297bdaca2dc66c4fe66393850480f4&language=fr&query=${targetId}&page=1&include_adult=false&primary_release_year=${searchByReleaseYear}`
-        //     );
-        // })
-    }
-
-    useEffect(() => {
-        const onSearchMovie = async () => {
-            if (searchByTitle) {
-                const result = await axios(
-                    `https://api.themoviedb.org/3/search/movie?api_key=6d297bdaca2dc66c4fe66393850480f4&language=fr&query=${searchByTitle}&page=1&include_adult=false&primary_release_year=${searchByReleaseYear}`
-                );
-                setData(result.data.results)
-            }
-            else {
-                setData(null)
-            }
-        }
-        onSearchMovie()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [searchByTitle])
-
-    useEffect(() => {
-        const abracadabra = async () => {
-            if (targetId) {
-                await axios(
-                    `https://api.themoviedb.org/3/search/movie?api_key=6d297bdaca2dc66c4fe66393850480f4&language=fr&query=${targetId}&page=1&include_adult=false&primary_release_year=${searchByReleaseYear}`
-                );
-                setSelectedMovie(data)
-                console.log(selectedMovie);
-            }
-        }
-        abracadabra()
+  useEffect(() => {
+    const onSearchMovie = async () => {
+      if (searchByTitle) {
+        const result = await axios(
+          `https://api.themoviedb.org/3/search/movie?api_key=6d297bdaca2dc66c4fe66393850480f4&language=fr&query=${searchByTitle}&page=1&include_adult=false&primary_release_year=${searchByReleaseYear}`
+        );
+        setData(result.data.results);
+      } else {
+        setData(null);
+      }
+    };
+    onSearchMovie();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [targetId])
+  }, [searchByTitle]);
 
+  useEffect(() => {
+    const abracadabra = async () => {
+      if (targetId) {
+        await axios(
+          `https://api.themoviedb.org/3/search/movie?api_key=6d297bdaca2dc66c4fe66393850480f4&language=fr&query=${targetId}&page=1&include_adult=false&primary_release_year=${searchByReleaseYear}`
+        );
+        setSelectedMovie(data);
+        console.log(selectedMovie);
+      }
+    };
+    abracadabra();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [targetId]);
 
-    return (
-        <Fragment>
-            <input
-                className='search-input'
-                onChange={e => setSearchByTitle(e.target.value)}
-                value={searchByTitle}
-                type='text'
-                placeholder="what's its title? you can type here..."
-            />
-            <input
-                className='search-input'
-                onChange={e => setSearchByReleaseYear(e.target.value)}
-                value={searchByReleaseYear}
-                type='text'
-                placeholder="you can precise the year of its release here!"
-            />
-            {data && data.length !== 0 && <ul>
-                {data?.map((dat, i) => {
-                    return (
-                        <li
-                            className='movie-result'
-                            key={i}
-                            id={dat.id}
-                            onClick={addData}>
-                            <p>{dat.title}</p>
-                            <p>{dat.release_date}</p>
-                        </li>
-                    )
-                })}
-            </ul>}
-            {/* {targetId &&
+  return (
+    <Fragment>
+      <input
+        className="search-input"
+        onChange={(e) => setSearchByTitle(e.target.value)}
+        value={searchByTitle}
+        type="text"
+        placeholder="what's its title? you can type here..."
+      />
+      <input
+        className="search-input"
+        onChange={(e) => setSearchByReleaseYear(e.target.value)}
+        value={searchByReleaseYear}
+        type="text"
+        placeholder="you can precise the year of its release here!"
+      />
+      {data && data.length !== 0 && (
+        <ul>
+          {data?.map((dat, i) => {
+            console.log(dat);
+            return (
+              <Link to={`/movie/${dat.id}`}>
+                <li
+                  className="movie-result"
+                  key={i}
+                  id={dat.id}
+                  //   onClick={addData}
+                >
+                  <p>{dat.title}</p>
+                  <p>{dat.release_date}</p>
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      )}
+      {/* {targetId &&
             <p>{targetId}</p>} */}
-        </Fragment>
-    )
-}
+    </Fragment>
+  );
+};
 
 export default SearchMovie;
